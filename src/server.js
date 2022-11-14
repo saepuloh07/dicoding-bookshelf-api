c5d148bab985b9d81c2cb9345e55fd7a6bc47ca5
muhamad saepuloh <muhamadsaepuloh07@gmail.com>
2022-11-14 11:47:30 +0700

    fix error deploy

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 3000,
-        // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '44b4268d-7893-4e1f-8ab9-0ac75f2165a1',
         routes: {
             cors: {
                 origin: ['*'],
